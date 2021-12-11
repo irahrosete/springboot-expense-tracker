ad54cc038ed718b07b3810cad7f6fc359b8e2fd8
irah rosete <irah.rosete@outlook.com>
2021-12-11 18:29:34 +1100

    add filter to grant access to restricted categories url

--- src/main/java/com/spring/springbootexpensetracker/SpringbootExpenseTrackerApplication.java
+++ src/main/java/com/spring/springbootexpensetracker/SpringbootExpenseTrackerApplication.java
@@ -1,12 +1,24 @@
 package com.spring.springbootexpensetracker;
 
+import com.spring.springbootexpensetracker.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootExpenseTrackerApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 }
---
+++ src/main/java/com/spring/springbootexpensetracker/resources/CategoryResource.java
@@ -0,0 +1,18 @@
+package com.spring.springbootexpensetracker.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryResource {
+
+    @GetMapping("")
+    public String getAllCategories(HttpServletRequest request) {
+        int userId = (Integer) request.getAttribute("userId");
+        return "Authenticated UserId: " + userId;
+    }
+}
